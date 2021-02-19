ee09ef5162c4f10045e795203b44316b4eca1d27
Al Avery <al.avery.dev@gmail.com>
2021-02-19 07:56:06 -0600

    remove /words part of words route

--- code/src/routes/words.js
+++ code/src/routes/words.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-    const { data } = await axios.get(`http://localhost:5000/words${req.url}`)
+    const { data } = await axios.get(`http://localhost:5000${req.url}`)
     console.log(data);
     res.send(data)   
 })
